0d03531610668dbe22c7dfc01d257b6a06455c97
Carol <ommasshi@gmail.com>
2021-10-14 18:50:24 -0300

    capítulo 6 do livro concluído

--- projetos/02-aplicando-testes-unitarios-em-uma-cli/__tests__/database/user/create.test.js
+++ projetos/02-aplicando-testes-unitarios-em-uma-cli/__tests__/database/user/create.test.js
@@ -1,24 +1,51 @@
-import { createUser } from '../../../src/database/user/create.js'
+import { createUser } from "../../../src/database/user/create.js";
 
-import *	as	file from '../../../src/database/file.js';
-import path from '../../../src/database/path.js';
+import * as file from "../../../src/database/file.js";
 
+import ROLES from "../../../src/constants/roles.js";
 
-jest.mock('../../../src/database/file.js')
-jest.mock('../../../src/database/path.js');
+jest.mock("../../../src/database/file.js");
+jest.mock("../../../src/database/path.js");
 
 const usuario = {
-  email: 'qualquer@email.com',
-  password: 'senha1234',
-  userName: 'usuárioQualquer',
-  name: 'Usuário',
-  lastName: 'Qualquer'
+  email: "qualquer@email.com",
+  password: "senha1234",
+  userName: "usuárioQualquer",
+  name: "Usuário",
+  lastName: "Qualquer",
 };
 
-it('Cria	usuário	corretamente', async () => {
-  file.loadDatabase.mockResolvedValueOnce([])
-  const user = await createUser(usuario)
+beforeEach(() => {
+  file.loadDatabase.mockResolvedValueOnce([]);
+});
+afterEach(() => {
+  jest.clearAllMocks();
+});
+afterAll(() => {
+  jest.restoreAllMocks();
+});
+it("Cria	usuário	corretamente", async () => {
+  const user = await createUser(usuario);
 
   expect(file.loadDatabase).toHaveBeenCalledTimes(1);
   expect(file.saveDatabase).toHaveBeenCalledTimes(1);
+  expect(file.saveDatabase).toHaveBeenCalledWith([user]);
+  expect(user).toEqual({
+    ...usuario,
+    uid: expect.any(String),
+    role: ROLES.USER,
+  });
+});
+it("Cria	usuário ADMIN	corretamente", async () => {
+  const user = await createUser({ ...usuario, role: ROLES.ADMIN });
+  expect.assertions(4);
+
+  expect(file.loadDatabase).toHaveBeenCalledTimes(1);
+  expect(file.saveDatabase).toHaveBeenCalledTimes(1);
+  expect(file.saveDatabase).toHaveBeenCalledWith([user]);
+  expect(user).toEqual({
+    ...usuario,
+    uid: expect.any(String),
+    role: ROLES.ADMIN,
+  });
 });
--- projetos/02-aplicando-testes-unitarios-em-uma-cli/__tests__/utils/logger.test.js
+++ projetos/02-aplicando-testes-unitarios-em-uma-cli/__tests__/utils/logger.test.js
@@ -1,33 +1,35 @@
-import logger from '../../src/utils/logger'
+import logger from "../../src/utils/logger";
 
-const spyLog = jest.spyOn(console, 'log').mockImplementation()
-const spyLogError = jest.spyOn(console, 'error').mockImplementation()
+const spyLog = jest.spyOn(console, "log").mockImplementation();
+const spyLogError = jest.spyOn(console, "error").mockImplementation();
 
 beforeEach(() => {
   // spyLog.mockClear()
   // spyLogError.mockClear()
-  jest.clearAllMocks()
-})
+  jest.clearAllMocks();
+});
 afterAll(() => {
-  console.log('não funciona')
-  jest.restoreAllMocks()
-  console.log('isso funciona')
-})
+  // console.log("não funciona");
+  jest.restoreAllMocks();
+  // console.log("isso funciona");
+});
 
-describe('Logger File', () => {
-
-  it('Funções de logging: log', () => {
-    logger.log('teste')
-
-    expect(spyLog).toHaveBeenCalledTimes(1);
-  });
-  it('Funções de logging: success', () => {
-    logger.success('teste')
+describe("Logger File", () => {
+  it("Funções de logging: log", () => {
+    logger.log("teste");
+    expect.assertions(1);
 
     expect(spyLog).toHaveBeenCalledTimes(1);
   });
-  it('Funções de logging: error', () => {
-    logger.error('teste')
+  it("Funções de logging: success", () => {
+    logger.success("teste");
+    expect.assertions(1);
+
+    expect(spyLog).toHaveBeenCalledTimes(1);
+  });
+  it("Funções de logging: error", () => {
+    logger.error("teste");
+    expect.assertions(1);
 
     expect(spyLogError).toHaveBeenCalledTimes(1);
   });
